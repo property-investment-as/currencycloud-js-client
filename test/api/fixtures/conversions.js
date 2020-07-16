8d1e85c3bec5e7116151f0664a372bb72c259860
Ed Addario <eaddario@hotmail.com>
2020-07-16 15:19:34 +0100

    Add ConversionDatePreference endpoint

--- test/api/fixtures/conversions.js
+++ test/api/fixtures/conversions.js
@@ -39,6 +39,41 @@ nock('https://devapi.currencycloud.com:443', {"encodedQueryParams": true})
         "updated_at": "2018-11-23T11:02:23+00:00",
         "mid_market_rate": "0.8036"
     });
+//Create Conversion with conversion date preference
+nock('https://devapi.currencycloud.com:443', {"encodedQueryParams": true})
+    .post('/v2/conversions/create', "buy_currency=EUR&sell_currency=GBP&fixed_side=buy&amount=10000&conversion_date_preference=earliest&reason=Testing&term_agreement=true")
+    .reply(200, {
+        "id": "d56d7553-19ab-4cde-b44b-79cac86989cb",
+        "settlement_date": "2020-05-19T13:30:00+00:00",
+        "conversion_date": "2020-05-19T00:00:00+00:00",
+        "short_reference": "20200519-XYLXJL",
+        "creator_contact_id": "42a6af4a-65b8-4721-43d9-7f395da2551e",
+        "account_id": "3f22044f-ae21-42a1-bc4f-cd0370b008a5",
+        "currency_pair": "EURGBP",
+        "status": "awaiting_funds",
+        "buy_currency": "EUR",
+        "sell_currency": "GBP",
+        "client_buy_amount": "1000.00",
+        "client_sell_amount": "805.90",
+        "fixed_side": "buy",
+        "core_rate": "0.8059",
+        "partner_rate": "",
+        "partner_status": "funds_arrived",
+        "partner_buy_amount": "0.00",
+        "partner_sell_amount": "0.00",
+        "client_rate": "0.8059",
+        "deposit_required": false,
+        "deposit_amount": "0.00",
+        "deposit_currency": "",
+        "deposit_status": "not_required",
+        "deposit_required_at": "",
+        "payment_ids": [],
+        "unallocated_funds": "1000.00",
+        "unique_request_id": null,
+        "created_at": "2020-05-19T12:31:43+00:00",
+        "updated_at": "2020-05-19T12:31:43+00:00",
+        "mid_market_rate": "0.8058"
+    });
 //Get conversion
 nock('https://devapi.currencycloud.com:443', {"encodedQueryParams": true})
     .post('/v2/conversions/create', "buy_currency=EUR&sell_currency=GBP&fixed_side=buy&amount=10000.23&reason=Settling%20invoices&term_agreement=true")
@@ -114,7 +149,7 @@ nock('https://devapi.currencycloud.com:443', {"encodedQueryParams": true})
 nock('https://devapi.currencycloud.com:443', {"encodedQueryParams": true})
     .post('/v2/conversions/create', "buy_currency=EUR&sell_currency=GBP&fixed_side=buy&amount=10000.23&reason=Settling%20invoices&term_agreement=true")
     .reply(200, {
-        "id": "ffdf0544-a868-488f-b096-effe8926a857",
+        "id": "339c30e2-eed6-4dc4-b043-354f6b3e605f",
         "settlement_date": "2018-11-27T14:30:00+00:00",
         "conversion_date": "2018-11-27T00:00:00+00:00",
         "short_reference": "20181123-FSFQLZ",
@@ -148,13 +183,13 @@ nock('https://devapi.currencycloud.com:443', {"encodedQueryParams": true})
 
 nock('https://devapi.currencycloud.com:443', {"encodedQueryParams": true})
     .get('/v2/conversions/find')
-    .query({"conversion_ids": ["ffdf0544-a868-488f-b096-effe8926a857"]})
+    .query({"conversion_ids[]": "339c30e2-eed6-4dc4-b043-354f6b3e605f"})
     .reply(200, {
         "conversions": [{
-            "id": "ffdf0544-a868-488f-b096-effe8926a857",
+            "id": "339c30e2-eed6-4dc4-b043-354f6b3e605f",
             "settlement_date": "2018-11-27T14:30:00+00:00",
             "conversion_date": "2018-11-27T00:00:00+00:00",
-            "short_reference": "20181123-FSFQLZ",
+            "short_reference": "20181123-VRQPFS",
             "creator_contact_id": "3cae081f-56f7-4f2d-8235-45713b592b43",
             "account_id": "c0ea266f-b969-4d61-9ca0-e790c79072e5",
             "currency_pair": "EURGBP",
@@ -178,8 +213,8 @@ nock('https://devapi.currencycloud.com:443', {"encodedQueryParams": true})
             "payment_ids": [],
             "unallocated_funds": "10000.23",
             "unique_request_id": null,
-            "created_at": "2018-11-23T11:02:26+00:00",
-            "updated_at": "2018-11-23T11:02:26+00:00",
+            "created_at": "2018-11-23T11:02:23+00:00",
+            "updated_at": "2018-11-23T11:02:23+00:00",
             "mid_market_rate": "0.8036"
         }],
         "pagination": {
